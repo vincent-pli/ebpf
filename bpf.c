5e47754819a8aaa8026f62f2e478952d1ac7deaa
pengli <justdoit.pli@gmail.com>
2022-01-24 14:53:47 +0800

    add more

--- bpf.c
+++ bpf.c
@@ -6,6 +6,7 @@
 #define __section(NAME)                  \
 	__attribute__((section(NAME), used))
 
+#define _htonl __builtin_bswap32
 /* Map for blocking IP addresses from userspace */
 struct bpf_map_def __section("maps") blocked_map = {
         .type = BPF_MAP_TYPE_HASH,
@@ -21,8 +22,8 @@ inline bool handle_pkt(struct __sk_buff *skb) {
     /* Load packet header */
     bpf_skb_load_bytes(skb, 0, &iph, sizeof(struct iphdr));
 
-    const char fmt_str[] = "hello world from ebpf!";
-    bpf_trace_printk(fmt_str, sizeof(fmt_str));
+    const char fmt_str[] = "hello world from ebpf! => %x\n";
+    bpf_trace_printk(fmt_str, sizeof(fmt_str), htonl(iph.saddr));
     /* Check if IPs are in "blocked" map */
     bool blocked = bpf_map_lookup_elem(&blocked_map, &iph.saddr) || bpf_map_lookup_elem(&blocked_map, &iph.daddr);
     /* Return whether it should be allowed or dropped */
